8c0205df7e1eee608694c9216bc57efe06ec69ae
MaksFullJs <peliskom@gmail.com>
2024-12-06 15:55:15 +0200

    updated url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
 export const getChats = async () => {
-    return await axios.get("http://localhost:5020/api/chats");
+    return await axios.get("https://autoresponcechat.onrender.com/api/chats");
 };
 
 export const createChat = async (chat) => {
-    return await axios.post("http://localhost:5020/api/chats", chat);
+    return await axios.post("https://autoresponcechat.onrender.com/api/chats", chat);
 };
 
 export const deleteChat = async (id) => {
-    await axios.delete(`http://localhost:5020/api/chat/${id}`);
+    await axios.delete(`https://autoresponcechat.onrender.com/api/chat/${id}`);
 };
 
 export const editChat = async (id, updatedChat) => {
-    return axios.put(`http://localhost:5020/api/chat/${id}`, updatedChat);
+    return axios.put(`https://autoresponcechat.onrender.com/api/chat/${id}`, updatedChat);
 };
 
 export const getChat = async (id) => {
-    return await axios.get(`http://localhost:5020/api/chat/${id}`);
+    return await axios.get(`https://autoresponcechat.onrender.com/api/chat/${id}`);
 };
 
 export const createMessage = async (chatId, sender, text) => {
     return await axios.post(
-        `http://localhost:5020/api/chat/${chatId}/messages`,
+        `https://autoresponcechat.onrender.com/api/chat/${chatId}/messages`,
         { chatId, sender, text }
     );
 };
